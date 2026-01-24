c3078cefde68e2d50e830f93ed2c3caeadb1daf2
bbranco8 <bbranco.780@gmail.com>
2026-01-24 20:06:49 +0000

    cultura design

--- JS/cultura.js
+++ JS/cultura.js
@@ -1,3 +1,5 @@
+/* HOVER ------------------------------------------------ */
+/* FILME */
 let imagens_filme = document.querySelectorAll('.artigo_filme img');
 
 imagens_filme.forEach(img => {
@@ -13,7 +15,7 @@ imagens_filme.forEach(img => {
   });
 });
 
-
+/* MUSICA */
 let imagens_musica = document.querySelectorAll('.artigo_musica img');
 
 imagens_musica.forEach(img => {
@@ -27,4 +29,33 @@ imagens_musica.forEach(img => {
   img.addEventListener('mouseleave', () => {
     img.src = original;
   });
+});
+
+
+
+/* DESIGN BACKGROUND -------------------------------------------------------- */
+document.addEventListener('DOMContentLoaded', () => {
+  let header = document.querySelector('header');
+  let design_section = document.getElementById('design');
+  let logo = document.getElementById('favicon');
+
+  let original_logo = '../LOGO/3m_logo.png';
+  let white_logo = '../LOGO/3m_logo_white.png';
+
+  let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        header.classList.add('white_header');
+        logo.src = white_logo;
+      } else {
+        header.classList.remove('white_header');
+        logo.src = original_logo;
+      }
+    });
+  }, {
+    root: null, // viewport
+    threshold: 0.9
+  });
+
+  observer.observe(design_section);
 });
