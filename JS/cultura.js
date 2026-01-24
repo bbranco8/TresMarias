800fd763cb0c01b384ac25b1d77a9ee1d50e92a7
bbranco8 <bbranco.780@gmail.com>
2026-01-24 16:29:23 +0000

    cultura musica funcional

--- JS/cultura.js
+++ JS/cultura.js
@@ -1,14 +1,30 @@
-let imagens = document.querySelectorAll('.artigo_filme img');
+let imagens_filme = document.querySelectorAll('.artigo_filme img');
 
-  imagens.forEach(img => {
-    let original = img.src;
-    let hover = img.dataset.hover;
+imagens_filme.forEach(img => {
+  let original = img.src;
+  let hover = img.dataset.hover;
 
-    img.addEventListener('mouseenter', () => {
-      img.src = hover;
-    });
-
-    img.addEventListener('mouseleave', () => {
-      img.src = original;
-    });
+  img.addEventListener('mouseenter', () => {
+    img.src = hover;
   });
+
+  img.addEventListener('mouseleave', () => {
+    img.src = original;
+  });
+});
+
+
+let imagens_musica = document.querySelectorAll('.artigo_musica img');
+
+imagens_musica.forEach(img => {
+  let original = img.src;
+  let hover = img.dataset.hover;
+
+  img.addEventListener('mouseenter', () => {
+    img.src = hover;
+  });
+
+  img.addEventListener('mouseleave', () => {
+    img.src = original;
+  });
+});
